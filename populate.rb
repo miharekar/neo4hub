c76c54ca2d84cd1f374bb8cd2478e24b80067658
Miha Rekar <info@mr.si>
2015-05-23 17:57:00 +0200

    Add all RubyC speakers

--- populate.rb
+++ populate.rb
@@ -7,5 +7,8 @@ auth = {username: ENV['NEO4J_USERNAME'], password: ENV['NEO4J_PASSWORD']}
 Neo4j::Session.open(:server_db, 'http://localhost:7474', basic_auth: auth)
 
 require 'importer'
-users = %w(mrfoto mfilej otobrglez bbatsov ahoward juliancheal benlovell sferik)
-Importer.new(users).import
+users = %w(
+  mrfoto mfilej otobrglez bbatsov ahoward juliancheal benlovell sferik
+  myabc bogdan listochkin le0pard petrokoriakin fxposter vladislav-gorbenko
+)
+Importer.new(users).import(empty:true)
