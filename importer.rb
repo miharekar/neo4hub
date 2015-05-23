7bcf96c24e7a43d5ac65c53e47cee0859c794b4f
Miha Rekar <info@mr.si>
2015-05-23 17:10:44 +0200

    Improve logic for creating relationships

--- importer.rb
+++ importer.rb
@@ -7,15 +7,17 @@ class Importer
 
   def initialize(users)
     @users = users
+    Octokit.auto_paginate = true
     @client = Octokit::Client.new(access_token: ENV['ACCESS_TOKEN'])
   end
 
   def import
-    empty_db
+    # empty_db
     users.each do |login|
+      p "Importing #{login}..."
       user = import_user(login)
-    #  import_followers(user)
-    #  import_followings(user)
+      import_followers(user)
+      import_followings(user)
       import_owned_repositories(user)
       import_organizations(user)
     end
@@ -24,7 +26,7 @@ class Importer
   private
 
   def empty_db
-    #User.delete_all
+    User.delete_all
     Repository.delete_all
     Organization.delete_all
   end
@@ -37,6 +39,7 @@ class Importer
   end
 
   def import_followers(user)
+    p '...followers'
     client.followers(user.login).each do |data|
       follower = User.find_or_create_by(login: data.login)
       create_followers(follower, user)
@@ -44,6 +47,7 @@ class Importer
   end
 
   def import_followings(user)
+    p '...followings'
     client.following(user.login).each do |data|
       following = User.find_or_create_by(login: data.login)
       create_followers(user, following)
@@ -56,11 +60,15 @@ class Importer
   end
 
   def import_owned_repositories(user)
+    p '...repositories'
     client.repos(user.login).each do |data|
-      Repository.find_or_create_by(full_name: data.full_name).tap do |repo|
+      Repository.find_or_create_by(full_name: data.full_name) do |repo|
         repo.from_github(data)
         user.create_rel('OWNS', repo)
-        create_source_repository(repo) if data.fork
+        if data.fork
+          source = create_source_repository(repo)
+          source.create_rel('FORK', repo)
+        end
       end
     end
   end
@@ -71,17 +79,17 @@ class Importer
       source.from_github(data)
       klass = data.owner.type.constantize
       owner = klass.find_or_create_by(login: data.owner.login)
-      source.create_rel('FORK', fork)
       owner.create_rel('OWNS', source)
     end
   end
 
   def import_organizations(user)
+    p '...organizations'
     client.organizations(user.login).each do |data|
-      Organization.find_or_create_by(login: data.login).tap do |org|
+      organization = Organization.find_or_create_by(login: data.login) do |org|
         org.from_github(data)
-        org.create_rel('MEMBER', user)
       end
+      organization.create_rel('MEMBER', user) unless org.members.include?(user)
     end
   end
 end
--- populate.rb
+++ populate.rb
@@ -1,10 +1,11 @@
 require 'bundler'
 Bundler.require
 $LOAD_PATH.unshift(*['.', 'models/'])
+Dotenv.load
 
 auth = {username: ENV['NEO4J_USERNAME'], password: ENV['NEO4J_PASSWORD']}
 Neo4j::Session.open(:server_db, 'http://localhost:7474', basic_auth: auth)
 
 require 'importer'
-users = %w(mrfoto mfilej otobrglez)
+users = %w(mrfoto mfilej otobrglez bbatsov ahoward juliancheal benlovell sferik)
 Importer.new(users).import
